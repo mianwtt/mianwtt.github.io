12cbb599ae24d911e83eecf547b6778d16f81287
mianwtt <134291028+mianwtt@users.noreply.github.com>
2025-06-23 20:31:51 +0800

    Create particles.js

---
+++ docs/assets/particles.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS.load('particles-js', '/assets/particles.json', function() {
+    console.log('particles.js config loaded');
+  });
+});
